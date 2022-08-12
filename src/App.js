3aa38a008ea8e3ddd88856d5f4193855dc7bab71
viggypoker <svig32@gmail.com>
2022-08-12 16:45:24 +0530

    commit 1

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
-const App = ()  => (
-  <div>
-    <p>
-      Hello world
-    </p>
-  </div>
+const Hello = (props) =>{
+  return (
+    <div>
+      <p>Hello {props.name}</p>
+    </div>
+  )
+}
+
+const App = ()  =>{
+  console.log('Hello from component')
+  return (
+    <>
+    <h1>Greetings</h1>
+    <Hello name="vignesh" />
+    <Hello  />
+    <Hello name="ddd" />
+  </>
 )
+}
 
 export default App
