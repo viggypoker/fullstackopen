7dd81e040ff7ded5b3bb82c7712979d8891a88af
viggypoker <svig32@gmail.com>
2022-08-17 16:26:52 +0530

    some changes made

--- src/App.js
+++ src/App.js
@@ -1,21 +1,54 @@
-const Hello = (props) =>{
+const Part = (props) =>{
+  return (
+    <p>
+      {props.part} {props.exercises}
+    </p>
+  )
+}
+
+const Header = (props) =>{
+    return (
+      <h1>{props.course}</h1>
+    )
+
+}
+
+const Content = (props) =>{
   return (
     <div>
-      <p>Hello {props.name}</p>
+      <Part exercises={props.exercises1} part={props.part1} />
+      <Part exercises={props.exercises2}  part={props.part2} />
+      <Part exercises={props.exercises3}  part={props.part3} />
+
     </div>
   )
 }
 
-const App = ()  =>{
-  console.log('Hello from component')
+const Total = (props) =>{
   return (
-    <>
-    <h1>Greetings</h1>
-    <Hello name="vignesh" />
-    <Hello  />
-    <Hello name="ddd" />
-  </>
-)
+    <p>Number of exercises {props.exercises1 + props.exercises2 + props.exercises3}</p>
+  )
+}
+
+const App = ()  =>{
+  const course ='Half Stack application development'
+  const part1 = 'Fundamentals of React'
+  const exercises1 = 10
+  const part2 = 'Using props to pass data'
+  const exercises2 = 7
+  const part3 = 'State of a component'
+  const exercises3 = 14
+  return (
+    <div>
+      <Header course={course}  />
+      <Content part1={part1}  part2={part2}  part3={part3} exercises1={exercises1} exercises2={exercises2} exercises3={exercises3}  />
+      <Total exercises1={exercises1} exercises2={exercises2} exercises3={exercises3}  />
+
+    </div>
+  )
+
+
+
 }
 
 export default App
